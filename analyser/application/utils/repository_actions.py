09c37a83c67a45e40ad9980ce63badd54f2955c3
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-09-10 15:57:25 +0100

    Update

--- analyser/application/utils/repository_actions.py
+++ analyser/application/utils/repository_actions.py
@@ -1,4 +1,5 @@
 """Repository actions."""
+
 from __future__ import annotations
 
 from pathlib import Path
@@ -9,7 +10,8 @@ from structlog import get_logger, stdlib
 logger: stdlib.BoundLogger = get_logger()
 DEFAULT_GLOBS_TO_EXCLUDE = [".git"]
 
-def remove_excluded_files(path_to_repo: str,globs_to_exclude: list[str] = DEFAULT_GLOBS_TO_EXCLUDE) -> None:
+
+def remove_excluded_files(path_to_repo: str, globs_to_exclude: list[str] = DEFAULT_GLOBS_TO_EXCLUDE) -> None:
     """Remove excluded files from the repository.
 
     Args:
@@ -24,5 +26,3 @@ def remove_excluded_files(path_to_repo: str,globs_to_exclude: list[str] = DEFAUL
             elif path.is_dir():
                 rmtree(path)
                 logger.debug("Removing directory", directory_path=path)
-
-
