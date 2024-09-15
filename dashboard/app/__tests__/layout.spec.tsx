b0f9d1b21d1a96e5a90d513b9a48025b5609d838
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-09-15 11:55:04 +0100

    Update

--- dashboard/app/__tests__/layout.spec.tsx
+++ dashboard/app/__tests__/layout.spec.tsx
@@ -1,12 +1,11 @@
-import {expect, test} from '@jest/globals';
+import { expect, test } from "@jest/globals";
+import { render } from "@testing-library/react";
 
-import {render} from '@testing-library/react';
+import Layout from "../layout";
 
-import Layout from '../layout';
-
-test('renders default layout', async () => {
+test("renders default layout", async () => {
   // Act
-  const {findByText} = render(<Layout children={<p>Hello World</p>} />); // eslint-disable-line react/no-children-prop
+  const { findByText } = render(<Layout children={<p>Hello World</p>} />); // eslint-disable-line react/no-children-prop
   // Assert
   const navbar = await findByText(/Overview/i);
   expect(navbar).toBeTruthy();
--- dashboard/app/__tests__/page.spec.tsx
+++ dashboard/app/__tests__/page.spec.tsx
@@ -1,12 +1,11 @@
-import {expect, test} from '@jest/globals';
+import { expect, test } from "@jest/globals";
+import { render } from "@testing-library/react";
 
-import {render} from '@testing-library/react';
+import Home from "../page";
 
-import Home from '../page';
-
-test('renders home page', async () => {
+test("renders home page", async () => {
   // Act
-  const {findByText} = render(<Home />);
+  const { findByText } = render(<Home />);
   // Assert
   const heading = await findByText(/Welcome to Next.js!/i);
   expect(heading).toBeTruthy();
