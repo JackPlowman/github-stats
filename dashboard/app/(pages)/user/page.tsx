c8dba098f228ffbba94df6d43b30fab8b7a718f6
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-15 19:07:01 +0000

    Improve User Page

--- dashboard/app/(pages)/user/page.tsx
+++ dashboard/app/(pages)/user/page.tsx
@@ -1,8 +1,32 @@
+"use client"
+import { Octokit } from "octokit";
+import { useEffect, useState } from "react";
+
 export default function UserPage() {
+  const [profile, setProfile] = useState<{ name: string; bio: string; avatar_url: string } | null>(null);
+
+  useEffect(() => {
+    const octokit = new Octokit();
+    octokit.request('GET /users/{username}', {
+      username: 'JackPlowman'
+    }).then(response => {
+      setProfile({
+        name: response.data.name || '',
+        bio: response.data.bio || '',
+        avatar_url: response.data.avatar_url
+      });
+    });
+  }, []);
+
+  if (!profile) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
-      <h1>User</h1>
-      <p>This is the user page</p>
+      <h1>{profile.name}</h1>
+      <p>{profile.bio}</p>
+      <img src={profile.avatar_url} alt={`${profile.name}'s avatar`} width="100" />
     </>
   );
 }
