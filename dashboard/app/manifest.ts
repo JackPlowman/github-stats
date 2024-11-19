2185308b762d1d88e304e753fb6c134912d39e77
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-19 21:08:53 +0000

    update

--- dashboard/app/manifest.ts
+++ dashboard/app/manifest.ts
@@ -1,20 +1,20 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'GitHub Stats Dashboard',
-    short_name: 'GH Stats Dashboard',
-    description: 'A dashboard to view Jack\'s GitHub stats',
-    start_url: '/github-stats',
-    display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#fff',
+    name: "GitHub Stats Dashboard",
+    short_name: "GH Stats Dashboard",
+    description: "A dashboard to view Jack's GitHub stats",
+    start_url: "/github-stats",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
       },
     ],
-  }
+  };
 }
