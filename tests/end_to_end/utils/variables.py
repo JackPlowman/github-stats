55cb41e0320839455d036e5437c1e752341440e9
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-12 16:19:49 +0000

    update

--- tests/end_to_end/utils/variables.py
+++ tests/end_to_end/utils/variables.py
@@ -4,4 +4,6 @@ GITHUB_PAGES_URL = "https://jackplowman.github.io/github-stats"
 
 PROJECT_URL = getenv("PROJECT_URL") if getenv("PROJECT_URL") else GITHUB_PAGES_URL
 SITEMAP_URL_PREFIX = GITHUB_PAGES_URL
-EXPECTED_XML_CONTENT_TYPE = "text/xml" if getenv("ENVIRONMENT") == "local" else "application/xml"
+EXPECTED_XML_CONTENT_TYPE = (
+    "text/xml" if getenv("ENVIRONMENT") == "local" else "application/xml"
+)
