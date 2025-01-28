1ca580baa19f283b2a59e19cbe68a49355d96d19
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-28 21:30:53 +0000

    update

--- dashboard/components/LanguagesBarChart.tsx
+++ dashboard/components/LanguagesBarChart.tsx
@@ -42,7 +42,7 @@ export function LanguagesBarChart({
   );
 
   return (
-    <Card>
+    <Card className="text-black flex flex-col bg-slate-100 dark:bg-slate-800">
       <CardHeader>
         <CardTitle>{"Repository Languages"}</CardTitle>
         <CardDescription>{"Language Distribution"}</CardDescription>
