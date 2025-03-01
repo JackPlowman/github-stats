ebec695006f413734da2c9eb0298398e0a98681f
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-03-01 11:16:26 +0000

    chore: Refactor Git Hooks (#496)
    # Pull Request
    
    ## Description
    
    This pull request introduces several changes to improve the pre-commit
    hooks, update dependencies, and add new format checks. The most
    important changes include the refactoring of pre-commit hooks to use
    `just` commands, the addition of `gitleaks` detection, and updates to
    the dependencies in `tests/pyproject.toml`.
    
    Refactoring pre-commit hooks:
    
    *
    [`githooks/pre-commit`](diffhunk://#diff-d5fe9d324948511c959e14d0aa311bb057ce7236626bb86fa51351dc26845a4dL4-R46):
    Refactored to run `gitleaks`, `prettier`, `just format`, Python, and
    TypeScript checks using `just` commands.
    *
    [`githooks/pre-commit-scripts/gitleaks.sh`](diffhunk://#diff-b488692093f21df73d5c8d67f3a6df3273b7199a34ca84e9c9e1a115b20df271L1-L4):
    Removed as its functionality is now integrated into the main pre-commit
    script.
    *
    [`githooks/pre-commit-scripts/justfile.sh`](diffhunk://#diff-fcd01eccc95d37bac6ab073a69ec60e574b754774360a13f31e1a39466668fc9L1-L4):
    Removed as its functionality is now integrated into the main pre-commit
    script.
    *
    [`githooks/pre-commit-scripts/prettier.sh`](diffhunk://#diff-1e2ab1eef74462625d345d9811c6cd8e51996878583f4a9878ad8d62ec7d014dL1-L4):
    Removed as its functionality is now integrated into the main pre-commit
    script.
    *
    [`githooks/pre-commit-scripts/python.sh`](diffhunk://#diff-f13793c94aec64ea3a3f3f3bcc4f045eeb9d66b5d895396175dd6f49ae3302b1L1-L8):
    Removed as its functionality is now integrated into the main pre-commit
    script.
    *
    [`githooks/pre-commit-scripts/typescript.sh`](diffhunk://#diff-934e2ddeffd2f8ce9741c68dd76115f93f7fbee76c53fe1176e8f5a0a25a52b2L1-L8):
    Removed as its functionality is now integrated into the main pre-commit
    script.
    
    Adding format checks:
    
    *
    [`Justfile`](diffhunk://#diff-2f90408c2b0302b1cdb7f5d634114750837f940fa82d13057d9c18d0170a7e5cR48-R54):
    Added `gitleaks-detect` command to check for leaks using `gitleaks`.
    *
    [`tests/tests.just`](diffhunk://#diff-83e2fb4528694cfa62c213eefec53e6aeee0353030bf91ad4f830e69854b7918R49-R53):
    Added `ruff-checks` command to check for all Ruff issues.
    
    Updating dependencies:
    
    *
    [`.github/actions/setup-test-dependencies/action.yml`](diffhunk://#diff-ee1e9a84357cfd23d067400706d92b6bb9d8fd8744e81563d9858e8c344df144L16-R16):
    Updated `astral-sh/setup-uv` from version `v5.2.2` to `v5.3.0`.
    *
    [`tests/pyproject.toml`](diffhunk://#diff-03049af3cb225ee5c5f22bc10731df89191cc762ad9ca9aa4cb7d3a87786ac8dL10-R19):
    Updated `selenium` to `4.29.0`, `ruff` to `0.9.9`, `zizmor` to `1.4.1`,
    and `uv` required version to `0.6.3`.

--- githooks/pre-commit-scripts/typescript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e +x
-
-# Check if there are any changes in the watched folder
-if git diff --cached --name-only | grep -q "^dashboard/"; then
-  just dashboard::install
-  just dashboard::lint
-fi
