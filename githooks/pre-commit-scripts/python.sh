6b86ea78dce6ac16691b1defe47b6db9ac6c753a
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-03-01 11:13:58 +0000

    update

--- githooks/pre-commit-scripts/python.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e +x
-
-# Check if there are any changes in the watched folder
-if git diff --cached --name-only | grep -q "^tests/"; then
-  just tests::install
-  just tests::ruff-fix
-fi
