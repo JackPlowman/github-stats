dc592838ba569669e129bb8e6baa1c96c7771268
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-03-06 21:23:12 +0000

    chore: Remove Pytest BDD Test Dependency (#512)
    # Pull Request
    
    ## Description
    
    This pull request includes significant changes to the end-to-end tests
    for the homepage and sitemap, as well as modifications to the
    dependencies in the `pyproject.toml` file. The main focus is on
    refactoring the tests to remove the use of `pytest-bdd` and simplifying
    the test structure.
    
    Refactoring end-to-end tests:
    
    * Removed `homepage.feature` file and its associated step definitions
    from `test_homepage.py`
    [[1]](diffhunk://#diff-5f94767a8075490f34031c9c7a0656e59b6d6772eac20b57e969f99a8872564eL1-L4)
    [[2]](diffhunk://#diff-742992d8e066bb34983abd597cc431b88b73f7177b4cec2b76fd9779d4646c0aL1-L18).
    * Removed `sitemap.feature` file and its associated step definitions
    from `test_sitemap.py`
    [[1]](diffhunk://#diff-eace947763caea7c4b2a51590c7a6e0c446c38676e2912086d501e546d91cdbbL1-L12)
    [[2]](diffhunk://#diff-6a27ed68cca1634d5b12218027d4d764418a5ac01008704ec2f42f0ac0bbb064L1-L121).
    * Added new test functions `test_homepage`, `test_sitemap_index`,
    `test_sitemap`, and `test_sitemap_urls` in `test_homepage.py` and
    `test_sitemap.py` to replace the removed BDD-style tests
    [[1]](diffhunk://#diff-05b4ab29c19ba99ffc33cc0a624eaafb52db2a05af0bf871067f13083a63d72fR1-R8)
    [[2]](diffhunk://#diff-ba4856c27cd44d538a01cd1404d46a9484ddd19c8af461a306825aac238fc621R1-R66).
    
    Dependency updates:
    
    * Removed `pytest-bdd` dependency from `pyproject.toml` as it is no
    longer used in the refactored tests.

---
+++ tests/end_to_end/test_sitemap.py
@@ -0,0 +1,66 @@
+from logging import getLogger
+
+from defusedxml.ElementTree import fromstring
+from pytest_bdd import scenarios
+from requests import get
+
+from end_to_end.utils.variables import (
+    EXPECTED_XML_CONTENT_TYPE,
+    PROJECT_URL,
+    SITEMAP_URL_PREFIX,
+)
+
+scenarios("../features/sitemap.feature")
+
+EXPECTED_SITEMAP_PATH = "sitemap-0.xml"
+EXPECTED_SITEMAP_URL = f"{PROJECT_URL}/{EXPECTED_SITEMAP_PATH}"
+logger = getLogger(__name__)
+
+
+def test_sitemap_index() -> None:
+    # Act
+    sitemap_index_response = get(f"{PROJECT_URL}/sitemap.xml", timeout=5)
+    assert sitemap_index_response.status_code == 200
+    # Assert
+    assert sitemap_index_response.headers["Content-Type"] == EXPECTED_XML_CONTENT_TYPE
+    sitemap_index_element = fromstring(sitemap_index_response.content)
+    assert (
+        sitemap_index_element.tag
+        == "{http://www.sitemaps.org/schemas/sitemap/0.9}sitemapindex"
+    )
+    sitemap_element = sitemap_index_element.find(
+        "{http://www.sitemaps.org/schemas/sitemap/0.9}sitemap"
+    )
+    loc_element = sitemap_element.find(
+        "{http://www.sitemaps.org/schemas/sitemap/0.9}loc"
+    ).text
+    assert loc_element == f"{SITEMAP_URL_PREFIX}/{EXPECTED_SITEMAP_PATH}"
+
+
+def test_sitemap() -> None:
+    # Act
+    sitemap_response = get(EXPECTED_SITEMAP_URL, timeout=5)
+    assert sitemap_response.status_code == 200
+    # Assert
+    assert sitemap_response.headers["Content-Type"] == EXPECTED_XML_CONTENT_TYPE
+    sitemap_element = fromstring(sitemap_response.content)
+    assert sitemap_element.tag == "{http://www.sitemaps.org/schemas/sitemap/0.9}urlset"
+
+
+def test_sitemap_urls() -> None:
+    # Act
+    # Act
+    sitemap_response = get(EXPECTED_SITEMAP_URL, timeout=5)
+    assert sitemap_response.status_code == 200
+
+    sitemap_element = fromstring(sitemap_response.content)
+    for url_element in sitemap_element.findall(
+        "{http://www.sitemaps.org/schemas/sitemap/0.9}url"
+    ):
+        loc_element = url_element.find(
+            "{http://www.sitemaps.org/schemas/sitemap/0.9}loc"
+        )
+        assert loc_element.text.startswith(SITEMAP_URL_PREFIX)
+        logger.debug(f"Requesting sitemap: {loc_element.text}")
+        response = get(loc_element.text, timeout=5)
+        assert response.status_code == 200
