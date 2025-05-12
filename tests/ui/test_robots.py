d00a5849d618416b0a6e2d8f2b049466a3d2bbc0
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-12 17:21:28 +0100

    update

--- tests/ui/test_robots.py
+++ tests/ui/test_robots.py
@@ -1,4 +1,3 @@
-
 from requests import get
 
 from ui.utils.variables import (
@@ -12,7 +11,7 @@ def test_robots_txt() -> None:
     robots_response = get(f"{PROJECT_URL}/robots.txt", timeout=5)
     assert robots_response.status_code == 200
     # Assert
-    assert robots_response.headers["Content-Type"] == "text/plain"
+    assert robots_response.headers["Content-Type"] == "text/plain; charset=utf-8"
     assert "User-agent: *" in robots_response.text
     assert "Disallow: /admin/" in robots_response.text
     assert "Allow: /admin/admin-ajax.php" in robots_response.text
