36f30c35270ca863a4c455e0ac8283ec2f7430ea
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-12 17:38:31 +0100

    update

--- tests/ui/test_robots.py
+++ tests/ui/test_robots.py
@@ -9,11 +9,12 @@ from ui.utils.variables import (
 
 def test_robots_txt() -> None:
     """Test the robots.txt file."""
+    # Arrange
+    with Path("ui/data/expected_robots.txt").open() as file:
+        expected_robots = file.read()
     # Act
     robots_response = get(f"{PROJECT_URL}/robots.txt", timeout=5)
     assert robots_response.status_code == 200
     # Assert
     assert robots_response.headers["Content-Type"] == "text/plain; charset=utf-8"
-    with Path("ui/data/expected_robots.txt").open() as file:
-        expected_robots = file.read()
     assert robots_response.text == expected_robots
