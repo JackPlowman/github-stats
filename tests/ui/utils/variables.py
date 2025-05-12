ed836682ea5ee73d1da25cf84402ca5be7726939
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-12 18:45:52 +0100

    update

--- tests/ui/utils/variables.py
+++ tests/ui/utils/variables.py
@@ -3,6 +3,8 @@ from os import getenv
 GITHUB_PAGES_URL = "https://jackplowman.github.io/github-stats"
 
 PROJECT_URL = getenv("PROJECT_URL") if getenv("PROJECT_URL") else GITHUB_PAGES_URL
+REPOSITORIES_URL = f"{PROJECT_URL}/repositories"
+
 SITEMAP_URL_PREFIX = GITHUB_PAGES_URL
 EXPECTED_XML_CONTENT_TYPE = (
     "text/xml" if getenv("ENVIRONMENT") == "local" else "application/xml"
