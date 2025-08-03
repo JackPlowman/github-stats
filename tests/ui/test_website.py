255d04c7132c4827f06882e761fae2a058e37932
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-03 13:39:26 +0100

    update

--- tests/ui/test_website.py
+++ tests/ui/test_website.py
@@ -1,10 +1,6 @@
-from logging import getLogger
-
 from playwright.sync_api import Page
 
 from ui.utils.variables import PROJECT_URL
-
-logger = getLogger(__name__)
 
 
 def test_title(page: Page) -> None:
