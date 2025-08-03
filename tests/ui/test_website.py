1db200e3f933b4ecd99e6aebb82b35b589171a4f
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-03 13:41:19 +0100

    chore: Fix UI Tests (#770)
    # Pull Request
    
    ## Description
    
    This pull request includes several changes aimed at simplifying the test
    suite and improving configuration clarity. The most significant updates
    involve removing redundant or outdated test cases, consolidating
    imports, and enhancing environment variable validation.
    
    ### Test Suite Simplifications:
    
    *
    [`tests/ui/test_repositories.py`](diffhunk://#diff-18684b3b8dbb8b4785694160f807327ff53dd63b4400dbad6159d04e8fff675aL1-L65):
    Removed the entire file, including tests for repository titles and
    content. These tests are no longer necessary due to changes in project
    scope or redundancy.
    *
    [`tests/ui/test_robots.py`](diffhunk://#diff-a1d7a40bfe8af0ec3abb73a7237dd5340576c0191aae15582826a68bd5576f82L5-R5):
    Consolidated imports by merging multi-line imports into a single line
    for better readability.
    
    ### Configuration Improvements:
    
    *
    [`tests/pyproject.toml`](diffhunk://#diff-03049af3cb225ee5c5f22bc10731df89191cc762ad9ca9aa4cb7d3a87786ac8dL41-L61):
    Removed the `exclude` and `dummy-variable-rgx` configurations,
    simplifying the linting setup. These settings are no longer required.
    *
    [`tests/ui/utils/variables.py`](diffhunk://#diff-4d14906db913f5e350b7c2b6fcd09e11ddf194348b52698293e3882a9130f5e4L6-R7):
    Added validation to ensure the `PROJECT_URL` environment variable is
    set, raising an error if it is missing. This change improves robustness.
    
    ### New Test Addition:
    
    *
    [`tests/ui/test_website.py`](diffhunk://#diff-3acf2f00d96a9ef81ec7a1b5c7109508abd39dc85da1bf88d79afc5c904fd644R1-R15):
    Introduced a new test to verify the website title, ensuring the page
    title matches "GitHub Stats Dashboard." This test replaces similar
    functionality removed from `test_repositories.py`.

---
+++ tests/ui/test_website.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import Page
+
+from ui.utils.variables import PROJECT_URL
+
+
+def test_title(page: Page) -> None:
+    """Test the website title."""
+    # Act
+    page.goto(PROJECT_URL)
+    # Assert
+    assert page.title() == "GitHub Stats Dashboard"
--- tests/ui/utils/variables.py
+++ tests/ui/utils/variables.py
@@ -3,7 +3,9 @@ from os import getenv
 GITHUB_PAGES_URL = "https://jackplowman.github.io/github-stats"
 
 PROJECT_URL = getenv("PROJECT_URL") if getenv("PROJECT_URL") else GITHUB_PAGES_URL
-REPOSITORIES_URL = f"{PROJECT_URL}/repositories"
+if not PROJECT_URL:
+    msg = "PROJECT_URL environment variable is not set."
+    raise ValueError(msg)
 
 SITEMAP_URL_PREFIX = GITHUB_PAGES_URL
 EXPECTED_XML_CONTENT_TYPE = (
