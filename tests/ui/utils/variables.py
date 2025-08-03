a544a0759904ed0d3da864c3667848387d5d801b
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-03 13:38:25 +0100

    update

--- tests/ui/utils/variables.py
+++ tests/ui/utils/variables.py
@@ -3,6 +3,8 @@ from os import getenv
 GITHUB_PAGES_URL = "https://jackplowman.github.io/github-stats"
 
 PROJECT_URL = getenv("PROJECT_URL") if getenv("PROJECT_URL") else GITHUB_PAGES_URL
+if not PROJECT_URL:
+    raise ValueError("PROJECT_URL environment variable is not set.")
 
 SITEMAP_URL_PREFIX = GITHUB_PAGES_URL
 EXPECTED_XML_CONTENT_TYPE = (
