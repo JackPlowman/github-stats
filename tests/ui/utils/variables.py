a5cf3f02a2fd4c5eec7638ab5692f642163d5ef6
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2026-02-03 19:38:18 +0000

    update

--- tests/ui/utils/variables.py
+++ tests/ui/utils/variables.py
@@ -2,7 +2,7 @@ from os import getenv
 
 GITHUB_PAGES_URL = "https://jackplowman.github.io/github-stats"
 
-PROJECT_URL = getenv("PROJECT_URL") if getenv("PROJECT_URL") else GITHUB_PAGES_URL
+PROJECT_URL = getenv("PROJECT_URL") or GITHUB_PAGES_URL
 if not PROJECT_URL:
     msg = "PROJECT_URL environment variable is not set."
     raise ValueError(msg)
